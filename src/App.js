fb8b885e621a160e5b4261e16d7abbf289160553
RichieRunner <richieho18@yahoo.com>
2018-06-05 01:15:28 -0700

    add moviedb api

--- src/App.js
+++ src/App.js
@@ -3,29 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    id: 1,
-    title: "Star Wars"
-  },
-  {
-    id: 2,
-    title: "Spiderman"
-  },
-  {
-    id: 3,
-    title: "The Room"
-  }
-];
-
 class App extends Component {
+
+  state = {
+    movies: []
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=9efcc7b3717c87ef6cfe11214da67368&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results
+      });
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
   render() {
+    console.log(this.state.movies);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie}/>
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie}/>
         )}
       </div>
     );
--- src/Movie.js
+++ src/Movie.js
@@ -5,18 +5,18 @@ export default class Movie extends Component {
   static propTypes = {
     movie: PropTypes.shape({
       title: PropTypes.string.isRequired,
-      desc: PropTypes.string.isRequired
+      // desc: PropTypes.string.isRequired
     })
   }
 
   static defaultProps = {
     
   }
+  
   render() {
     return (
       <div>
         <h3>{this.props.movie.title}</h3>
-        <p>{this.props.movie.desc}</p>
       </div>
     )
   }
